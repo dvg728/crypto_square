f67fed124c6fe8c976bb4a9d9165018b6521ebae
dvg728 <gordondv@dukes.jmu.edu>
2016-04-08 10:20:36 -0400

    Added files via upload

---
+++ crypto_square/crypto-square.js
@@ -0,0 +1,13 @@
+// create our Crypto class
+//it takes a string as an argument
+var Crypto = function(text) {
+	// store the string for use elsewhere
+	this.text = text;
+};
+
+Crypto.prototype.normalizePlaintext = function() {
+	return this.toLowerCase().replace(/\W/g "#$%^");
+
+};
+
+module.exports = Crypto;
