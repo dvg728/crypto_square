9e9495b94f8f5d73fdab14175d3c923a9428b715
dvg728 <gordondv@dukes.jmu.edu>
2016-04-20 10:12:15 -0400

    Added files via upload

--- crypto_square/crypto-square.js
+++ crypto_square/crypto-square.js
@@ -1,13 +1,105 @@
 // create our Crypto class
-//it takes a string as an argument
+// it takes a string as an argument
 var Crypto = function(text) {
-	// store the string for use elsewhere
-	this.text = text;
+	this.message = text;
 };
 
 Crypto.prototype.normalizePlaintext = function() {
-	return this.toLowerCase().replace(/\W/g "#$%^");
-
+	return this.message.replace(/[\W]/g, "").toLowerCase();
 };
+
+Crypto.prototype.size = function() {
+	var length = this.normalizePlaintext().length;
+	return Math.ceil(Math.sqrt(length));
+};
+
+Crypto.prototype.plaintextSegments = function() {
+	var segments = [],
+		npt = this.normalizePlaintext(),
+		size = this.size();
+
+	for (var i = 0; i < npt.length; i += size) {
+		// extract segments from the normalized plain text
+		// and add it to the segements array
+		segments.push(npt.substr(i, size));
+	}
+
+	// return an array of strings representing
+	// segments of the normalized plain text
+	return segments;
+};
+
+Crypto.prototype.ciphertext = function() {
+	var ct = "",
+		size = this.size(), // the number of columns
+		segs = this.plaintextSegments(); // the rows
+
+	// loop through the columns
+	for (var i = 0; i < size; i += 1) {
+		// loop through the rows
+		for (var j = 0; j < segs.length; j += 1) {
+			ct += segs[j].charAt(i);
+		}
+	}
+
+	// returns a string that represents
+	// the cipher text, i.e. the encoded message
+	return ct;
+};
+
+module.exports = Crypto;
+
+
+
+
+
+/*
+// create our Crypto class
+//it takes a string as an argument
+var Crypto = function(text) {
+	this.message = text;
+};
+
+Crypto.prototype.normalizePlaintext = function() {
+	return this.message.replace(/[\W]/g, "").toLowerCase();
+};
+
+Crypto.prototype.size = function() {
+	var length = this.normalizePlaintext().length;
+	return math.ceil(Math.sqrt(length));
+};
+
+Crypto.Prototype.plaintextSegments = function() {
+	var segments = [],
+		npt = this.normalizePlaintext(),
+		size = this.size();
+	
+	for (var i = 0; i != npt.length; i += size; ) {
+		
+	// extract segments from the normalized plain text
+	// and add it to the segments array
+		segments.push(npt.subst(0, size));
+	}
+	// return an array of string representing
+	//segments of the normalized plain text
+	return segments;
+}
+
+Crypto.prototype.ciphertext = function() {
+	var ct = "";
+		size = this.size(),
+		segs = this.plaintextSegments();
+	// loop thorought the columns
+	for (var i = 0; i < size; i += 1) {
+		//loop through the rows
+		for (var j = 0; j < segs.length; j += 1) {
+			ct += segs[j].charAt(i);
+		}
+	}
+
+	// returns a string that reprents
+	// the cipher text, i.e. the encoded message
+	return ct;
+}
 
 module.exports = Crypto;
